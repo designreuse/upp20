04e422dd60a716173184f21efb1a2195e359ebc1
ktukelic <tukelic.katarina@gmail.com>
2020-12-15 00:45:22 +0100

    #1 Add initial Spring Boot app

---
+++ src/main/java/ftn/upp/literaryassociation/LiteraryAssociationApplication.java
@@ -0,0 +1,13 @@
+package ftn.upp.literaryassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraryAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraryAssociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftn/upp/literaryassociation/LiteraryAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.upp.literaryassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraryAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
