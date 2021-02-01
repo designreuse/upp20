832bad1a6816a5bdd786e590b552c320e8f52664
Filip Ivkovic <fivkovic@uns.ac.rs>
2021-02-01 12:55:07 +0100

    Add initial implementation of e-mail notification service [WIP] #12
    Co-Authored-By: Katarina Tukelić <tukelic.katarina@gmail.com>

--- backend/src/main/java/org/ftn/upp/literaryassociation/LiteraryAssociationApplication.java
+++ backend/src/main/java/org/ftn/upp/literaryassociation/LiteraryAssociationApplication.java
@@ -2,8 +2,10 @@ package org.ftn.upp.lass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LiteraryAssociationApplication {
 
 	public static void main(String[] args) { SpringApplication.run(LiteraryAssociationApplication.class, args); }
