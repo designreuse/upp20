9176fb6416b35599f785c68d3150e497b7a0d1a1
Filip Ivkovic <fivkovic@uns.ac.rs>
2021-02-01 21:18:20 +0100

    Updated process management controller closes #21
    Co-Authored-By: Katarina Tukelić <tukelic.katarina@gmail.com>

--- backend/src/main/java/org/ftn/upp/lass/controller/ProcessManagementController.java
+++ backend/src/main/java/org/ftn/upp/lass/controller/ProcessManagementController.java
@@ -2,8 +2,8 @@ package org.ftn.upp.lass.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.exception.NullValueException;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.ftn.upp.lass.common.LogMessages;
 import org.ftn.upp.lass.common.api.RestApiEndpoints;
@@ -11,7 +11,7 @@ import org.ftn.upp.lass.common.api.RestApiRequestParameters;
 import org.ftn.upp.lass.dto.response.ProcessInfoResponse;
 import org.ftn.upp.lass.exception.NotFoundException;
 import org.ftn.upp.lass.util.ErrorMessageUtil;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,21 +23,21 @@ public class ProcessManagementController {
     private final RuntimeService runtimeService;
 
     @GetMapping(RestApiEndpoints.START_PROCESS_INSTANCE)
-    public ResponseEntity<ProcessInfoResponse> startProcessInstance(@RequestParam(RestApiRequestParameters.PROCESS_NAME) String processName) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public ProcessInfoResponse startProcessInstance(@RequestParam(RestApiRequestParameters.PROCESS_NAME) String processName) {
         log.info(LogMessages.STARTING_PROCESS, processName);
 
         final ProcessInstance processInstance;
         try {
             processInstance = this.runtimeService.startProcessInstanceByKey(processName);
-        } catch (NullValueException e) {
+        } catch (ProcessEngineException e) {
             throw new NotFoundException(ErrorMessageUtil.processWithGivenNameNotFound(processName));
         }
 
-        var responseBody = ProcessInfoResponse.builder()
+        log.info(LogMessages.PROCESS_STARTED, processInstance.getId(), processName);
+        return ProcessInfoResponse.builder()
                 .processInstanceId(processInstance.getId())
                 .build();
-
-        log.info(LogMessages.PROCESS_STARTED, processInstance.getId(), processName);
-        return ResponseEntity.ok(responseBody);
     }
 }
