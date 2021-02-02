3f3862f53fe8f2e0d560d59926f845f7a41b0872
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-02-02 11:54:15 +0100

    Implemented username availability validator #24
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- backend/src/main/java/org/ftn/upp/lass/repository/UserRepository.java
+++ backend/src/main/java/org/ftn/upp/lass/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findUserByUsername(String username);
+    boolean existsUserByUsername(String username);
 }
