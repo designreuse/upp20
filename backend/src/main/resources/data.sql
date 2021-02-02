287aebb415043d02de4b443310726a97d23f6666
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-02-02 19:56:07 +0100

    Add predefined genres to default data SQL script
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- TRUNCATE TABLE literary_association_db.genres ^;
+INSERT INTO literary_association_db.genres (id, name)
+VALUES
+    (9000, 'Action'), (9001, 'Adventure'), (9002, 'Classic'), (9003, 'Comic Book'), (9004, 'Graphic Novel'),
+    (9005, 'Mystery'), (9006, 'Fantasy'), (9007, 'Historical Fiction'), (9008, 'Horror'), (9009, 'Romance'),
+    (9010, 'Literary Fiction'), (9011, 'SciFi'), (9012, 'Short Stories'), (9013, 'Thriller'), (9014, 'Biography'),
+    (9015, 'Cookbook'), (9016, 'Memoir'), (9017, 'History'), (9018, 'Poetry'), (9019, 'Self Help'), (9020, 'True Crime') ^;
+
+
