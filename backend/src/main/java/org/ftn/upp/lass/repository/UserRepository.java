77fbb161fc4b9275284dab023dffb1f6a67e6eb0
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-02-02 11:54:32 +0100

    Implemented email availability validator #24
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- backend/src/main/java/org/ftn/upp/lass/repository/UserRepository.java
+++ backend/src/main/java/org/ftn/upp/lass/repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findUserByUsername(String username);
     boolean existsUserByUsername(String username);
+    boolean existsUserByEmail(String email);
 }
