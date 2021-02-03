bae3a6efa3f1eed4c22b1f0ab53b94ecbed44cd3
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-02-03 15:53:15 +0100

    Implemented delegate for sending verification email #24
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- backend/src/main/java/org/ftn/upp/lass/integration/delegate/SendAccountVerificationNotification.java
+++ backend/src/main/java/org/ftn/upp/lass/integration/delegate/SendAccountVerificationNotification.java
@@ -4,17 +4,28 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.ftn.upp.lass.common.Constants;
+import org.ftn.upp.lass.common.LogMessages;
+import org.ftn.upp.lass.model.User;
+import org.ftn.upp.lass.service.NotificationService;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class SendAccountVerificationNotification implements JavaDelegate {
+
+    private final NotificationService notificationService;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        log.info("EXECUTE SendAccountVerificationNotification");
+        log.info(LogMessages.EXECUTE, SendAccountVerificationNotification.class.getName());
 
-        // TODO: Implement
+        User user = (User) execution.getVariable(Constants.ProcessVariables.REGISTERED_READER);
+        this.notificationService.sendVerificationEmail(user);
+
+        log.info(LogMessages.FINISHED, SendAccountVerificationNotification.class.getName());
     }
 }
