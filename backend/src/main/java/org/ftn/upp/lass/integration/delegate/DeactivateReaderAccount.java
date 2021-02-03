043c14ab41099b93514cefd4589eb14584a4449c
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-02-03 15:52:38 +0100

    Implemented delegate for reader and beta access reader account deactivation #24
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- backend/src/main/java/org/ftn/upp/lass/integration/delegate/DeactivateReaderAccount.java
+++ backend/src/main/java/org/ftn/upp/lass/integration/delegate/DeactivateReaderAccount.java
@@ -4,17 +4,36 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.ftn.upp.lass.common.Constants;
+import org.ftn.upp.lass.common.LogMessages;
+import org.ftn.upp.lass.model.MembershipStatus;
+import org.ftn.upp.lass.model.Reader;
+import org.ftn.upp.lass.repository.UserRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class DeactivateReaderAccount implements JavaDelegate {
+
+    private final UserRepository userRepository;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        log.info("EXECUTE DeactivateReaderAccount");
+        log.info(LogMessages.EXECUTE, DeactivateReaderAccount.class.getName());
 
-        // TODO: Implement
+        final var username = (String) execution.getVariable(Constants.FormFieldVariables.USERNAME);
+        final var userOptional = this.userRepository.findUserByUsername(username);
+        if (userOptional.isPresent()) {
+            final var reader = (Reader) userOptional.get();
+            reader.setIsVerified(false);
+            reader.setMembershipStatus(MembershipStatus.INACTIVE);
+
+            this.userRepository.save(reader);
+        }
+
+        log.info(LogMessages.FINISHED, DeactivateReaderAccount.class.getName());
     }
 }
