7e64887a57157cabd64ab76c645fceb50191f475
Filip Ivkovic <fivkovic@uns.ac.rs>
2021-02-05 11:50:30 +0100

    Implemented author account deactivation delegate #27

---
+++ backend/src/main/java/org/ftn/upp/lass/integration/delegate/DeactivateAuthorAccount.java
@@ -0,0 +1,40 @@
+package org.ftn.upp.lass.integration.delegate;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.ftn.upp.lass.common.Constants;
+import org.ftn.upp.lass.common.LogMessages;
+import org.ftn.upp.lass.model.Author;
+import org.ftn.upp.lass.model.MembershipStatus;
+import org.ftn.upp.lass.repository.UserRepository;
+import org.ftn.upp.lass.service.VerificationCodeService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class DeactivateAuthorAccount implements JavaDelegate {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        log.info(LogMessages.EXECUTE, DeactivateAuthorAccount.class.getName());
+
+        final var username = (String) execution.getVariable(Constants.FormFieldVariables.USERNAME);
+        final var userOptional = this.userRepository.findUserByUsername(username);
+        if (userOptional.isPresent()) {
+            final var author = (Author) userOptional.get();
+            author.setIsVerified(false);
+            author.setMembershipStatus(MembershipStatus.INACTIVE);
+
+            this.userRepository.save(author);
+        }
+
+        log.info(LogMessages.FINISHED, DeactivateAuthorAccount.class.getName());
+    }
+}
